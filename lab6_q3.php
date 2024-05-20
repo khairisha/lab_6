8f8c07f7cf31df8ed8702e09510da522be69ae98
Nurul Khairisha Afza <khairishafzakhairuddin@gmail.com>
2024-05-20 21:08:33 +0800

    change done

---
+++ lab6_q3.php
@@ -0,0 +1,13 @@
+<?php
+function calculateArea($length, $width) 
+{
+    $area = $length * $width;
+    return $area;
+}
+
+$length = 4; 
+$width = 2; 
+$area = calculateArea($length, $width);
+
+echo "The area of the rectangle with length $length and width $width is: $area";
+?>
